0440236c7f81d931e9f7633e37e234ad1c83339a
Nahid <n.shahab85@gmail.com>
2023-05-27 15:30:30 +0330

    create react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+
+export default function Weather() {
+  const [city, setCity] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+    const units = "metric";
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  const form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={changeCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
