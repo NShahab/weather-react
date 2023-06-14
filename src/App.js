bb278e4f8efdbf0c068e48648b3f3375606e7bbc
Nahid <n.shahab85@gmail.com>
2023-06-14 09:19:08 +0330

    api key was edited

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Paris" />
 
         <footer>
           This project was coded by Nahid Shahab and is{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
